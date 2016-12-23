a80b4b23cd1748df10a134407e1ad736ed852488
Adrian <adrian@byvapps.com>
2016-12-23 13:26:53 +0100

    UIColor change

--- ByvUtils/Classes/Utils.swift
+++ ByvUtils/Classes/Utils.swift
@@ -26,35 +26,4 @@ public struct Utils {
         UserDefaults.standard.set([code], forKey: "AppleLanguages")
         UserDefaults.standard.synchronize()
     }
-    
-    public static func json(_ data: Data?) -> [String: Any] {
-        if let _data = data {
-            do {
-                if let anyObj = try JSONSerialization.jsonObject(with: _data) as? [String: Any] {
-                    return anyObj
-                }
-            } catch {
-                print("json error: \(error.localizedDescription)")
-            }
-        }
-        return [:]
-    }
-    
-    public static func jsonArray(_ data: Data?) -> Array<[String: Any]> {
-        if let _data = data {
-            do {
-                if let anyArray = try JSONSerialization.jsonObject(with: _data) as? Array<[String: Any]> {
-                    return anyArray
-                }
-            } catch {
-                print("json error: \(error.localizedDescription)")
-            }
-        }
-        return Array()
-    }
-    
-    public static func cleanJson(_ json:[String: Any], withObject: Any? = nil) -> [String: Any] {
-        
-        return json
-    }
 }
