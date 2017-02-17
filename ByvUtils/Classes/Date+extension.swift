9833bbaa213c028c20a2f65ea697970c3b4d2291
Adrian <adrian@byvapps.com>
2017-02-17 14:04:58 +0100

    New functions

---
+++ ByvUtils/Classes/Date+extension.swift
@@ -0,0 +1,22 @@
+//
+//  Date+extension.swift
+//  Pods
+//
+//  Created by Adrian Apodaca on 17/2/17.
+//
+//
+
+import Foundation
+
+extension Date {
+    public static func fromMilliseconds(_ milliseconds: Int?) -> Date? {
+        if let millis = milliseconds {
+            return Date(timeIntervalSince1970: Double(millis/1000))
+        }
+        return nil
+    }
+    
+    public func milliseconds() -> Double {
+        return self.timeIntervalSince1970 * 1000
+    }
+}
